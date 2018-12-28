8f42469a4f3f11fe02e49c64c5faca7b6b9306c9
Alex <quacksterr.15@gmail.com>
2018-12-28 21:56:03 +1000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -424,3 +424,177 @@ namespace SpriteConverter
     }
 }
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpriteConverter
+{
+class Program
+{
+    class FurniItem
+    {
+        public string Type;
+        public string SpriteId;
+        public string FileName;
+        public string Revision;
+        public string Unknown;
+        public int Length;
+        public int Width;
+        public string Colour;
+        public string Name;
+        public string Description;
+
+        public FurniItem(string[] data)
+        {
+            this.Type = data[0];
+            this.SpriteId = data[1];
+            this.FileName = data[2];
+            this.Revision = data[3];
+            this.Unknown = data[4];
+            try
+            {
+                this.Length = Convert.ToInt32(data[5]);
+                this.Width = Convert.ToInt32(data[6]);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            this.Colour = data[7];
+            this.Name = data[8];
+            this.Description = data[9];
+        }
+
+        class ProductItem
+        {
+            public string FileName;
+            public string Name;
+            public string Description;
+
+            public ProductItem(string[] data)
+            {
+                this.FileName = data[0];
+                this.Name = data[1];
+                this.Description = data[2];
+            }
+        }
+
+        class CatalogueItem
+        {
+            public string FileName;
+            public int ItemId;
+
+            public CatalogueItem(string fileName, int itemId)
+            {
+                FileName = fileName;
+                ItemId = itemId;
+            }
+        }
+
+        private static MySqlConnectionStringBuilder holoDbString;
+        private static MySqlConnectionStringBuilder keplerDbString;
+
+        private static Dictionary<String, int> maxInteractions;
+        private static Dictionary<int, List<CatalogueItem>> catalogueItems;
+
+        static void Main(string[] args)
+        {
+            maxInteractions = new Dictionary<string, int>();
+
+            Console.WriteLine("Converter for furnidata.txt");
+
+            string fileContents = File.ReadAllText("furnidata.txt");
+            var furnidataList = JsonConvert.DeserializeObject<List<string[]>>(fileContents);
+
+            List<FurniItem> itemList = new List<FurniItem>();
+            List<ProductItem> productList = new List<ProductItem>();
+
+            catalogueItems = new Dictionary<int, List<CatalogueItem>>();
+
+            foreach (var stringArray in furnidataList)
+                itemList.Add(new FurniItem(stringArray));
+
+            Dictionary<int, string> posterDataName = new Dictionary<int, string>();
+            Dictionary<int, string> posterDataDesc = new Dictionary<int, string>();
+
+            keplerDbString = new MySqlConnectionStringBuilder();
+            keplerDbString.Server = "localhost";
+            keplerDbString.Port = 3306;
+            keplerDbString.UserID = "root";
+            keplerDbString.Password = "123";
+            keplerDbString.Database = "keplerdev";
+            keplerDbString.MinimumPoolSize = 5;
+            keplerDbString.MaximumPoolSize = 10;
+            keplerDbString.SslMode = MySqlSslMode.None;
+
+
+            int counter = 0;
+            string line;
+
+            System.IO.StreamReader file = new System.IO.StreamReader("external_texts.txt");
+
+            while ((line = file.ReadLine()) != null)
+            {
+                MySqlConnection conn = new MySqlConnection(keplerDbString.ToString());
+                conn.Open();
+
+                if (line.StartsWith("poster_") && line.Contains("_name"))
+                {
+                    int posterId = int.Parse(line.Split('_')[1]);
+                    posterDataName.Add(posterId, line.Trim().Replace("poster_" + posterId + "_name=", ""));
+
+
+                    MySqlCommand cmd = new MySqlCommand("UPDATE catalogue_items SET name = @name WHERE sale_code = @sale_code AND item_specialspriteid = @item_specialspriteid", conn);
+                    cmd.Parameters.AddWithValue("@item_specialspriteid", posterId);
+                    cmd.Parameters.AddWithValue("@name", posterDataName[posterId]);
+                    cmd.Parameters.AddWithValue("@sale_code", "poster");
+                    cmd.ExecuteNonQuery();
+                }
+
+                if (line.StartsWith("poster_") && line.Contains("_desc"))
+                {
+                    int posterId = int.Parse(line.Split('_')[1]);
+                    posterDataDesc.Add(posterId, line.Trim().Replace("poster_" + posterId + "_desc=", ""));
+
+                    MySqlCommand cmd = new MySqlCommand("UPDATE catalogue_items SET description = @description WHERE sale_code = @sale_code AND item_specialspriteid = @item_specialspriteid", conn);
+                    cmd.Parameters.AddWithValue("@item_specialspriteid", posterId);
+                    cmd.Parameters.AddWithValue("@description", posterDataDesc[posterId]);
+                    cmd.Parameters.AddWithValue("@sale_code", "poster");
+                    cmd.ExecuteNonQuery();
+                }
+
+
+
+                conn.Close();
+                counter++;
+            }
+
+            file.Close();
+
+            Console.WriteLine("Finished");
+        }
+    }
+}
+}
+*/
